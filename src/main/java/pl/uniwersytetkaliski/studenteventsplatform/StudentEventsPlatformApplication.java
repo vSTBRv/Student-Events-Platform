c2546a098b068e7c9d06db2ed960e99ec3106711
vSTBRv <vSTBRv@gmail.com>
2025-03-31 13:31:33 +0200

    Base structure

---
+++ src/main/java/pl/uniwersytetkaliski/studenteventsplatform/StudentEventsPlatformApplication.java
@@ -0,0 +1,13 @@
+package pl.uniwersytetkaliski.studenteventsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentEventsPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentEventsPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/uniwersytetkaliski/studenteventsplatform/StudentEventsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uniwersytetkaliski.studenteventsplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentEventsPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
