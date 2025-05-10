9a0a950193af7347a56f24512a3490b94d5d29b8
vSTBRv <vSTBRv@gmail.com>
2025-05-10 20:43:30 +0200

    naprawiono baze

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -12,8 +12,8 @@ INSERT INTO locations (city, street, house_number, postal_code) VALUES
                                                                   ('Kraków', 'Wielopole', '15A', '31-072'),
                                                                   ('Gdańsk', 'Długa', '22', '80-827');
 INSERT INTO events (name, location_id, status, max_capacity,current_capacity, creation_date, start_date, end_date, description, category_id, deleted, deleted_at, created_by) VALUES
-                                                                                                            ('Hackathon Uczelniany', 1, 'PLANNED',0, 50, CURRENT_TIMESTAMP, '2025-05-10 09:00:00', '2025-05-10 18:00:00', '24h kodowania, pizza, nagrody!',1,0,NULL,3),
-                                                                                                            ('Wieczór planszówek', 2, 'PLANNED',0, 30, CURRENT_TIMESTAMP, '2025-05-12 17:30:00', '2025-05-12 22:00:00', 'Integracja i gry planszowe dla studentów.',2,1,CURRENT_TIMESTAMP,3),
-                                                                                                            ('Bieg po Kampusie', 3, 'PLANNED', 0,100, CURRENT_TIMESTAMP, '2025-05-15 12:00:00', '2025-05-15 14:00:00', 'Zawody biegowe dla każdego poziomu.',3,0,NULL,1);
+                                                                                                            ('Hackathon Uczelniany', 1, 'PLANNED',50, 50, CURRENT_TIMESTAMP, '2025-05-10 09:00:00', '2025-05-10 18:00:00', '24h kodowania, pizza, nagrody!',1,0,NULL,3),
+                                                                                                            ('Wieczór planszówek', 2, 'PLANNED',30, 30, CURRENT_TIMESTAMP, '2025-05-12 17:30:00', '2025-05-12 22:00:00', 'Integracja i gry planszowe dla studentów.',2,1,CURRENT_TIMESTAMP,3),
+                                                                                                            ('Bieg po Kampusie', 3, 'PLANNED', 100,100, CURRENT_TIMESTAMP, '2025-05-15 12:00:00', '2025-05-15 14:00:00', 'Zawody biegowe dla każdego poziomu.',3,0,NULL,1);
 
 INSERT INTO user_event (user_id, event_id) VALUES (1,1)
