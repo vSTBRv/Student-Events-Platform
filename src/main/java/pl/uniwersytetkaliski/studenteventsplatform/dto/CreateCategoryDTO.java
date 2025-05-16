6871e6c0146faff06191c44c3c2cd664fe505503
vSTBRv <vSTBRv@gmail.com>
2025-05-16 14:33:03 +0200

    Utworzenie dodatkowych DTO oraz mappera dla kategori

---
+++ src/main/java/pl/uniwersytetkaliski/studenteventsplatform/dto/CreateCategoryDTO.java
@@ -0,0 +1,18 @@
+package pl.uniwersytetkaliski.studenteventsplatform.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public class CreateCategoryDTO {
+    @NotBlank
+    @Pattern(regexp = "^\\D*$")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
