44d800cc575d979c482cc2d1c2733174f414c7be
vSTBRv <vSTBRv@gmail.com>
2025-05-17 12:47:15 +0200

    poprawienie bazy danych

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -5,16 +5,16 @@ insert into users (username, email, password, full_name, user_role, enabled, cre
 insert into users (username, email, password, full_name, user_role, enabled, created_at) values ('admin','admin@mail.to', '$2a$12$woCGm7dBTeJh8aoK0RFHtO3ePy6hfYCG.nYWGbkS1XQ8Loe0Cg8WO', 'testAdmin','ADMIN',1,CURRENT_TIMESTAMP );
 insert into users (username, email, password, full_name, user_role, enabled, created_at) values ('org','org@mail.to' ,'$2a$10$mOqJBpHZuFbff.M2utwUpOpgBrbYLdTI2.y4pPpkCinpiwq0e22U6', 'testOrg','ORGANIZATION',1,CURRENT_TIMESTAMP );
 
-INSERT INTO category (name) VALUES ('test1'),('test2'),('test3');
+INSERT INTO category (name,deleted) VALUES ('test1',0),('test2',0),('test3',0);
 
 INSERT INTO locations (city, street, house_number, postal_code) VALUES
                                                                   ('Warszawa', 'Koszykowa', '86', '00-123'),
                                                                   ('Kraków', 'Wielopole', '15A', '31-072'),
                                                                   ('Gdańsk', 'Długa', '22', '80-827');
 INSERT INTO events (name, location_id, status, max_capacity,current_capacity, creation_date, start_date, end_date, description, category_id, deleted, deleted_at, created_by, accepted) VALUES
-                                                                                                            ('Hackathon Uczelniany', 1, 'PLANNED',50, 50, CURRENT_TIMESTAMP, '2025-05-10 09:00:00', '2025-05-10 18:00:00', '24h kodowania, pizza, nagrody!',1,0,NULL,3,1),
-                                                                                                            ('Wieczór planszówek', 2, 'PLANNED',30, 30, CURRENT_TIMESTAMP, '2025-05-12 17:30:00', '2025-05-12 22:00:00', 'Integracja i gry planszowe dla studentów.',2,1,CURRENT_TIMESTAMP,3,1),
-                                                                                                            ('Bieg po Kampusie', 3, 'PLANNED', 100,100, CURRENT_TIMESTAMP, '2025-05-15 12:00:00', '2025-05-15 14:00:00', 'Zawody biegowe dla każdego poziomu.',3,0,NULL,1,1),
-                                                                                                            ('Event do zaakceptowania', 3, 'PLANNED', 100,100, CURRENT_TIMESTAMP, '2025-05-15 12:00:00', '2025-05-15 14:00:00', 'Testowy event wymagajacy zaakceptowania',3,0,NULL,1,0);
+                                                                                                            ('Hackathon Uczelniany', 1, 'PLANNED',50, 0, CURRENT_TIMESTAMP, '2025-05-10 09:00:00', '2025-05-10 18:00:00', '24h kodowania, pizza, nagrody!',1,0,NULL,3,1),
+                                                                                                            ('Wieczór planszówek', 2, 'PLANNED',30, 0, CURRENT_TIMESTAMP, '2025-05-12 17:30:00', '2025-05-12 22:00:00', 'Integracja i gry planszowe dla studentów.',2,1,CURRENT_TIMESTAMP,3,1),
+                                                                                                            ('Bieg po Kampusie', 3, 'PLANNED', 100,0, CURRENT_TIMESTAMP, '2025-05-15 12:00:00', '2025-05-15 14:00:00', 'Zawody biegowe dla każdego poziomu.',3,0,NULL,1,1),
+                                                                                                            ('Event do zaakceptowania', 3, 'PLANNED', 100,0, CURRENT_TIMESTAMP, '2025-05-15 12:00:00', '2025-05-15 14:00:00', 'Testowy event wymagajacy zaakceptowania',3,0,NULL,1,0);
 
 INSERT INTO user_event (user_id, event_id) VALUES (1,1)
