f437eab00c9471163ba7522602638f4101c9e499
Michał <mcichy4@gmail.com>
2025-06-07 22:37:01 +0200

    Raport liczby uczestników wydarzeń - tylko dla zaakceptowanych i nieusuniętych wydarzeń

---
+++ src/main/java/pl/uniwersytetkaliski/studenteventsplatform/dto/reportDTO/ParticipantCountReportDTO.java
@@ -0,0 +1,17 @@
+package pl.uniwersytetkaliski.studenteventsplatform.dto.reportDTO;
+
+public class ParticipantCountReportDTO {
+    private long participantCount;
+
+    public ParticipantCountReportDTO(long participantCount) {
+        this.participantCount = participantCount;
+    }
+
+    public long getParticipantCount() {
+        return participantCount;
+    }
+
+    public void setParticipantCount(long participantCount) {
+        this.participantCount = participantCount;
+    }
+}
